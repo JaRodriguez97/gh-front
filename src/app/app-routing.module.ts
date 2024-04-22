20117a807dd2daa020c4e18669a652f8a4a882bf
JaRodriguez97 <cristi542012@gmail.com>
2024-04-21 19:13:26 -0500

    Title corregido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     path: 'servicio-grua-cali-economicas-24-horas',
     loadChildren: () =>
       import('./modules/Landing/landing.module').then((m) => m.LandingModule),
-    title: 'Servicio Grúa Cali Economicas 24 Horas - Grúas CL',
+    title: 'Servicio Grúa Cali Economicas 24 Horas - Grúas Higuita',
   },
   { path: '**', component: NotFoundComponent },
 ];
